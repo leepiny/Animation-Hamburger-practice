f60f6ca38af584f32702ea6785d34d7d3e2a5344
leepiny <57059608+leepiny@users.noreply.github.com>
2020-12-06 15:40:26 +0800

    animation-hamburger-practice

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('.btn-range').click(function (e) { 
+        e.preventDefault();
+        $('.btn-range').toggleClass('open');
+    });
+});
+
+// var range = document.querySelector('.btn-range');
+
+// range.addEventListener('click',(e =>{
+//     range.classList.add('open');
+// }))
+
